d3a62339b7c6260e58545e619d31a13cf7498e1a
Samuel Beryl Enrico Ritonga <2010631170119@student.unsika.ac.id>
2023-09-29 09:23:34 +0700

    renaming import PageLayout from Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Carousel from "../components/Carousel";
-import PageLayout from "../components/layouts/pageLayout";
+import PageLayout from "../components/layouts/PageLayout";
 import Card from "../components/Card";
 import { Flex } from "@chakra-ui/react";
 import MenuCard from "../components/MenuCard";
