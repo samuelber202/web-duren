1d88b550c6fb4262bbbaed686949826e9de6b561
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-10-01 12:43:04 +0700

    informasi page

---
+++ src/components/GrayBox.jsx
@@ -0,0 +1,27 @@
+import { Box, Flex, Text } from '@chakra-ui/react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function GrayBox() {
+  return (
+    <>
+    
+      <Box
+        width="100%"
+        position="absolute"
+        bg="#E4E6EB"
+        height={'300px'}
+        zIndex={'-99'}
+        color="white"
+        mb={10}
+      />
+           
+     
+
+  
+    
+    </>
+  )
+}
+
+export default GrayBox
